8f3352d004ae60e0738ca4b7af214cbc3d79d534
benliuss <2931915202@qq.com>
2018-02-05 22:54:29 +0800

    feat: 增加antd-mobile样式按需加载

--- src/App.js
+++ src/App.js
@@ -1,18 +1,15 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { Button, List } from 'antd-mobile'
 
-class App extends Component {
+class App extends React.Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+        <Button type='warning'>确定</Button>
+        <List renderHeader={()=>'列表'}>
+          <List.Item>1</List.Item>
+          <List.Item>2</List.Item>
+        </List>
       </div>
     );
   }
